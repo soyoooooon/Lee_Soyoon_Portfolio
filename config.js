95a967c39c042a21245746e9204e645491e5df7d
이소윤 <soyoon@isoyuns-MacBook-Pro.local>
2019-12-08 02:21:23 -0500

    heroku..

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'db_soyoon',
-    port: 8889
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'bfe17091fe4b6a',
+    password: 'b76deb76',
+    database: 'heroku_bcb5f5ad54eb156'
+   
    }
    
    module.exports = config;
