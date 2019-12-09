371786d4e6a26f344c34379665cfe0726d994ce1
이소윤 <s_lee77176@fanshaweonline.ca>
2019-12-08 20:35:40 -0500

    my laptop is being slow.. due to this project

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,43 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 const connect = require('../utils/sql');
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', function(req, res, next) {
 
-  // get the connection via the connection pool, and then run the query -> just one added step
   connect.getConnection((err, connection) => {
-  if (err) { return console.log(error.message); }
+		if (err) { return console.log(error.message); }
 
   let query = `SELECT ID,job,feature,extra FROM tbl_bio`;
 
   connect.query(query, (err, result) => {
-    connection.release(); // send this connection back to the pool 
+    connection.release();
+    if (err) { return console.log(err.message);}
+    console.log(result);
 
-    if (err) {
-      
-      return console.log(err.message);
-    }
-
-    // console.log(result); 
-
-    // render our page
+    
     res.render('index', {bio: result}); 
   });
+  });
 });
-})
+
 
 router.get('/info/:target', (req, res) => {
   
-  connect.getConnection((err, connection) => {
-    if (err) { return console.log(error.message); }
 
   let query = `SELECT * FROM portfolio WHERE id="${req.params.target}"`;
 
   connect.query(query, (err, result) => {
-    connection.release();
-    if (err) { console.log(err); }
-
+    // connection.release();
+    if (err) { return console.log(err.message);}
     console.log(result);
+
     
     res.json(result[0]);
-  })
+  });
 });
-});
+
 
 module.exports = router;
